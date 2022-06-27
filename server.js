c48eb04d7c90e4d3f738dd144dd4df9705cbeb94
Caleb Phillips <csphillips5@gmail.com>
2022-06-27 16:11:57 -0400

    implement /registration route handler for POST req

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
+const User = require('./models/User');
 const app = express();
 
 mongoose.connect(process.env.DB_CONNECTION_STRING);
@@ -11,6 +12,25 @@ mongoose.connect(process.env.DB_CONNECTION_STRING);
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/registration', (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save(err => {
+    if (err) {
+      return res.status(400).json({
+        title: 'error',
+        error: 'Email already in use',
+      });
+    }
+    return res.status(200).json({
+      title: 'User successfully registered',
+    });
+  });
+});
 
 const PORT = process.env.PORT || 8000;
 
