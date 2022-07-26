a6ac3e90b573734ea24b4160853a502662c65f60
Caleb Phillips <csphillips5@gmail.com>
2022-07-26 16:44:16 -0400

    save production build to be served up using express.static('build')

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ mongoose.connect(process.env.DB_CONNECTION_STRING);
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/registration', (req, res) => {
